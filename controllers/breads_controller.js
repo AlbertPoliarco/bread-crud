050a992c230344ae05ed5e89252bd9bfd57b300e
AlbertPoliarco <nikpoliarco@outlook.com>
2021-12-06 23:25:18 -0600

    show all breads

---
+++ controllers/breads_controller.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bread = require("../models/bread");
+const breads = express.Router();
+const Bread = require("../models/bread")
+
+breads.get("/", (req, res) => {
+    res.send(Bread);
+});
+
+
+breads.get("/:arrayIndex", (req, res) => {
+    res.send(Bread[req.params.arrayIndex])
+})
+module.exports = breads;
